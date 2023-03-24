727b6b50b98e58d0cb050541fd7f47f5dcf0b2e9
eu10 <eu10@18m.dev>
2023-03-24 09:36:54 +0000

    update build performance

--- fake-blocklag/handler.go
+++ fake-blocklag/handler.go
@@ -8,8 +8,7 @@ import (
 	"math/rand"
 	"net/http"
 	"os"
-
-	"github.com/Jeffail/gabs"
+	// "github.com/Jeffail/gabs"
 )
 
 func Handle(w http.ResponseWriter, r *http.Request) {
@@ -59,54 +58,54 @@ func Fetch(endpoint string) []byte {
 	return body
 }
 
-func Test2(data []byte) string {
-	jsonParsed, err := gabs.ParseJSON(data)
-	if err != nil {
-		panic(err)
-	}
-	height := jsonParsed.Path("height").Data()
-	fmt.Println(jsonParsed)
-	fmt.Println(height)
-	jsonParsed1, _ := gabs.ParseJSON([]byte(`{"height":123}`))
-	jsonParsed.Delete("height")
-	jsonParsed.Merge(jsonParsed1)
-	fmt.Println(jsonParsed)
-	return jsonParsed.String()
-}
+// func Test2(data []byte) string {
+// 	jsonParsed, err := gabs.ParseJSON(data)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	height := jsonParsed.Path("height").Data()
+// 	fmt.Println(jsonParsed)
+// 	fmt.Println(height)
+// 	jsonParsed1, _ := gabs.ParseJSON([]byte(`{"height":123}`))
+// 	jsonParsed.Delete("height")
+// 	jsonParsed.Merge(jsonParsed1)
+// 	fmt.Println(jsonParsed)
+// 	return jsonParsed.String()
+// }
 
-func Test1() {
-	data := []byte(`{
-		"employees":{
-		   "protected":false,
-		   "address":{
-			  "street":"22 Saint-Lazare",
-			  "postalCode":"75003",
-			  "city":"Paris",
-			  "countryCode":"FRA",
-			  "country":"France"
-		   },
-		   "employee":[
-			  {
-				 "id":1,
-				 "first_name":"Jeanette",
-				 "last_name":"Penddreth"
-			  },
-			  {
-				 "id":2,
-				 "firstName":"Giavani",
-				 "lastName":"Frediani"
-			  }
-		   ]
-		}
-	 }`)
-	jsonParsed, err := gabs.ParseJSON(data)
-	if err != nil {
-		panic(err)
-	}
+// func Test1() {
+// 	data := []byte(`{
+// 		"employees":{
+// 		   "protected":false,
+// 		   "address":{
+// 			  "street":"22 Saint-Lazare",
+// 			  "postalCode":"75003",
+// 			  "city":"Paris",
+// 			  "countryCode":"FRA",
+// 			  "country":"France"
+// 		   },
+// 		   "employee":[
+// 			  {
+// 				 "id":1,
+// 				 "first_name":"Jeanette",
+// 				 "last_name":"Penddreth"
+// 			  },
+// 			  {
+// 				 "id":2,
+// 				 "firstName":"Giavani",
+// 				 "lastName":"Frediani"
+// 			  }
+// 		   ]
+// 		}
+// 	 }`)
+// 	jsonParsed, err := gabs.ParseJSON(data)
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	// Search JSON
-	fmt.Println("Get value of Protected:\t", jsonParsed.Path("employees.protected").Data())
-	fmt.Println("Get value of Country:\t", jsonParsed.Search("employees", "address", "country").Data())
-	fmt.Println("ID of first employee:\t", jsonParsed.Path("employees.employee.0.id").String())
-	fmt.Println("Check Country Exists:\t", jsonParsed.Exists("employees", "address", "countryCode"))
-}
+// 	// Search JSON
+// 	fmt.Println("Get value of Protected:\t", jsonParsed.Path("employees.protected").Data())
+// 	fmt.Println("Get value of Country:\t", jsonParsed.Search("employees", "address", "country").Data())
+// 	fmt.Println("ID of first employee:\t", jsonParsed.Path("employees.employee.0.id").String())
+// 	fmt.Println("Check Country Exists:\t", jsonParsed.Exists("employees", "address", "countryCode"))
+// }
